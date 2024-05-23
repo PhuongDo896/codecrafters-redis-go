72ac16fbd51896c79d4d8b579872af5e02edf0cf
Phuong Do <minhphuong.dh96@gmail.com>
2024-05-23 23:18:18 +0700

    spawn goroutine for each connection

--- app/server.go
+++ app/server.go
@@ -23,21 +23,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 
-	for {
-		b := make([]byte, 1024)
-		_, err = conn.Read(b)
-		if err != nil {
-			panic(err)
-		}
+	// for {
+	// 	b := make([]byte, 1024)
+	// 	_, err = conn.Read(b)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
 
-		if strings.Contains(string(b), "PING") {
-			conn.Write([]byte(RESPONSE))
-		}
+	// 	if strings.Contains(string(b), "PING") {
+	// 		conn.Write([]byte(RESPONSE))
+	// 	}
+	// }
+}
+
+func handleConnection(conn net.Conn) {
+	b := make([]byte, 1024)
+	_, err := conn.Read(b)
+	if err != nil {
+		panic(err)
+	}
+
+	if strings.Contains(string(b), "PING") {
+		conn.Write([]byte(RESPONSE))
 	}
 }

2209846bf358aaed52adcbe678a95bd77e74e6a9
PhuongDo <minhphuong.dh96@gmail.com>
2024-05-25 21:08:08 +0700

    implement echo command

--- app/server.go
+++ app/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"regexp"
+	"strconv"
 	"strings"
 
 	// Uncomment this block to pass the first stage
@@ -12,7 +14,9 @@ import (
 	"os"
 )
 
-const RESPONSE = "+PONG\r\n"
+const (
+	RESPONSE = "+PONG\r\n"
+)
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -44,9 +48,9 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	for {
-		b := make([]byte, 1024)
+		input := make([]byte, 1024)
 		// TODO: this block until new line is sent by client
-		_, err := conn.Read(b)
+		_, err := conn.Read(input)
 		if errors.Is(err, io.EOF) {
 			log.Println("Connection closed by client")
 			break
@@ -55,8 +59,58 @@ func handleConnection(conn net.Conn) {
 			break
 		}
 
-		if strings.Contains(string(b), "PING") {
+		if strings.Contains(string(input), "PING") {
 			conn.Write([]byte(RESPONSE))
 		}
+
+		commands := respParser(string(input))
+		if len(commands) != 2 {
+			continue
+		}
+
+		if commands[0] == "echo" {
+			conn.Write(response(commands[1]))
+		}
 	}
+}
+
+func respParser(resp string) []string {
+	// normalize input
+	resp = strings.ToLower(resp)
+
+	re := regexp.MustCompile(`\$([0-9]+)\r\n(.+?)\r\n`)
+	match := re.FindAllString(resp, -1)
+
+	command := make([]string, 0)
+
+	for _, m := range match {
+		word, ok := processBulkString(m)
+		if ok {
+			command = append(command, word)
+		}
+	}
+
+	return command
+}
+
+// 1st bit: $
+// 2nd bit: word length
+// 3rd and 4th bit: \r\n
+// 2 last bits: \r\n
+// => total bits = word length + 6
+func processBulkString(s string) (string, bool) {
+	wordLen, err := strconv.Atoi(string(s[1]))
+	if err != nil {
+		return "", false
+	}
+
+	if len(s) != wordLen+6 {
+		return "", false
+	}
+
+	return s[4 : 4+wordLen], true
+}
+
+func response(s string) []byte {
+	return []byte(fmt.Sprintf("+%s\r\n", s))
 }
